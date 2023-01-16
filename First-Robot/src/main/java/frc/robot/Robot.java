f1d1f81d47db29a753955f046467a654249545d2
Lee Zenke <lee.zenke@zenkeconsulting.tech>
2023-01-16 08:34:26 -0600

    Added AHRS and some vendor library updates

--- First-Robot/src/main/java/frc/robot/Robot.java
+++ First-Robot/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import java.util.Map;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -21,6 +22,8 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -36,11 +39,18 @@ public class Robot extends TimedRobot {
   private DifferentialDrive m_robotDrive;
   private final XboxController m_controller = new XboxController(0);
   private final Timer m_timer = new Timer();
+  private AHRS ahrs;
 
-    // Maps for XBox Xontroller Diag Screen
-  private GenericEntry jstick1_xaxis, jstick1_yaxis, jstick2_xaxis, jstick2_yaxis, leftTrigger, rightTrigger,
-    xButton, yButton, bButton, aButton, backButton, startButton, leftBumper, rightBumper, jstick1_click,
-    jstick2_click, dpadPOV, leftRumbleToggle, leftRumbleValue, rightRumbleToggle, rightRumbleValue;
+    // Maps for XBox Controller Diag Screen
+    private GenericEntry jstick1_xaxis, jstick1_yaxis, jstick2_xaxis, jstick2_yaxis, leftTrigger, rightTrigger,
+        xButton, yButton, bButton, aButton, backButton, startButton, leftBumper, rightBumper, jstick1_click,
+        jstick2_click, dpadPOV, leftRumbleToggle, leftRumbleValue, rightRumbleToggle, rightRumbleValue;
+        
+    // Maps for Navigation Diag Screen
+    private GenericEntry ahrsConnected, ahrsCalibrating, accelermoterXAxis, accelermoterYAxis, accelermoterZAxis,
+        ahrsIsMoving, ahrsIsRotating, ahrsAccelX, ahrsAccelY, gyroYaw, gyroPitch, gyroRoll, velocityX, velocityY,
+        leftEncoderPulseCount, leftEncoderVelocity, leftCountsPerRev, rightEncoderPulseCount, 
+        rightEncoderVelocity, rightCountsPerRev;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -61,7 +71,15 @@ public class Robot extends TimedRobot {
 
     m_robotDrive = new DifferentialDrive(m_leftLeadDrive, m_rightLeadDrive);
 
-    initHMI();
+    try {
+        ahrs = new AHRS(SPI.Port.kMXP); 
+    } catch (RuntimeException ex ) {
+        DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
+    }
+
+    initXboxControllerHMI();
+    initNavigationHMI();
+
 
   }
 
@@ -75,6 +93,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {    
+    updateXboxControllerHMI();
+    updateNavigationHMI();
   }
 
   /** This function is called once each time the robot enters teleoperated mode. */
@@ -84,7 +104,9 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY() *.5, -m_controller.getRightX() * .5);
+    updateXboxControllerHMI();
+    updateNavigationHMI();
+    m_robotDrive.arcadeDrive(-m_controller.getLeftY() *.7, -m_controller.getLeftX() * .7);
   }
 
   /** This function is called once each time the robot enters test mode. */
@@ -94,6 +116,11 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
+    updateXboxControllerHMI();
+    updateNavigationHMI();
+  }
+
+  private void updateXboxControllerHMI() {
     jstick1_xaxis.setDouble(m_controller.getLeftX());
     jstick1_yaxis.setDouble(m_controller.getLeftY());
     jstick2_xaxis.setDouble(m_controller.getRightX());
@@ -126,10 +153,9 @@ public class Robot extends TimedRobot {
     else{
       m_controller.setRumble(RumbleType.kRightRumble,0);
     }
-    
   }
 
-  private void initHMI() {
+  private void initXboxControllerHMI() {
     ShuffleboardTab controllerDiagTab = Shuffleboard.getTab("Controller Diag");
 
     ShuffleboardLayout leftJoystickLayout = controllerDiagTab.getLayout("Left Joystick", "Grid Layout")
@@ -198,4 +224,84 @@ public class Robot extends TimedRobot {
         .withPosition(0, 1).getEntry();
   }
 
+  private void initNavigationHMI() {
+    ShuffleboardTab driveDiagTab = Shuffleboard.getTab("Drive Diag");
+
+    ahrsConnected = driveDiagTab.add("AHRS Connected", false).withWidget(BuiltInWidgets.kBooleanBox).withPosition(0, 0)
+        .withSize(1, 1).withProperties(Map.of("color when true", "Green", "color when false", "Black")).getEntry();
+
+    ahrsCalibrating = driveDiagTab.add("AHRS Calibrating", false).withWidget(BuiltInWidgets.kBooleanBox)
+        .withPosition(1, 0).withSize(1, 1)
+        .withProperties(Map.of("color when true", "Green", "color when false", "Black")).getEntry();
+
+    ShuffleboardLayout accelerometerLayout = driveDiagTab.getLayout("Accelerometers", "Grid Layout").withPosition(0, 1)
+        .withSize(1, 3).withProperties(Map.of("number of columns", 1, "number of rows", 3));
+    accelermoterXAxis = accelerometerLayout.add("X Axis", 0).withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 0)
+        .withProperties(Map.of("min", -1, "max", 1)).getEntry();
+    accelermoterYAxis = accelerometerLayout.add("Y Axis", 0).withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 0)
+        .withProperties(Map.of("min", -1, "max", 1)).getEntry();
+    accelermoterZAxis = accelerometerLayout.add("Z Axis", 0).withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 0)
+        .withProperties(Map.of("min", -1, "max", 1)).getEntry();
+
+    ShuffleboardLayout gyroLayout = driveDiagTab.getLayout("Headings", "Grid Layout").withPosition(1, 1).withSize(1, 3)
+        .withProperties(Map.of("number of columns", 1, "number of rows", 3));
+    gyroYaw = gyroLayout.add("Yaw", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 0).getEntry();
+    gyroPitch = gyroLayout.add("Pitch", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 1).getEntry();
+    gyroRoll = gyroLayout.add("Roll", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 2).getEntry();
+
+    ahrsIsMoving = driveDiagTab.add("Is Moving", false).withWidget(BuiltInWidgets.kBooleanBox).withPosition(2, 0)
+        .withSize(1, 1).withProperties(Map.of("color when true", "Green", "color when false", "Black")).getEntry();
+
+    ahrsIsRotating = driveDiagTab.add("Is Rotating", false).withWidget(BuiltInWidgets.kBooleanBox).withPosition(3, 0)
+        .withSize(1, 1).withProperties(Map.of("color when true", "Green", "color when false", "Black")).getEntry();
+
+    velocityX = driveDiagTab.add("Velocity X", "").withWidget(BuiltInWidgets.kTextView).withPosition(2, 2).getEntry();
+    velocityY = driveDiagTab.add("Velocity Y", "").withWidget(BuiltInWidgets.kTextView).withPosition(3, 2).getEntry();
+    
+    ahrsAccelX = driveDiagTab.add("World Linear Accel X", "").withWidget(BuiltInWidgets.kTextView).withPosition(2, 1).getEntry();
+    ahrsAccelY = driveDiagTab.add("World Linear Accel Y", "").withWidget(BuiltInWidgets.kTextView).withPosition(3, 1).getEntry();
+
+    ShuffleboardLayout leftMotorLayout = driveDiagTab.getLayout("Left", "Grid Layout").withPosition(4, 1)
+    .withSize(2, 2).withProperties(Map.of("number of columns", 1, "number of rows", 3));
+    leftEncoderPulseCount = leftMotorLayout.add("Encoder Pulse Count", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 0).getEntry();
+    leftEncoderVelocity = leftMotorLayout.add("Encoder Velocity", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 1).getEntry();
+    leftCountsPerRev = leftMotorLayout.add("Counts Per Rev", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 2).getEntry();
+
+    ShuffleboardLayout rightMotorLayout = driveDiagTab.getLayout("Right", "Grid Layout").withPosition(6, 1)
+    .withSize(2, 2).withProperties(Map.of("number of columns", 1, "number of rows", 3));
+    rightEncoderPulseCount = rightMotorLayout.add("Encoder Pulse Count", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 0).getEntry();
+    rightEncoderVelocity = rightMotorLayout.add("Encoder Velocity", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 1).getEntry();
+    rightCountsPerRev = rightMotorLayout.add("Counts Per Rev", "").withWidget(BuiltInWidgets.kTextView).withPosition(0, 2).getEntry();
+    
+  }
+
+  private void updateNavigationHMI() {
+    ahrsConnected.setBoolean(ahrs.isConnected());
+    ahrsCalibrating.setBoolean(ahrs.isCalibrating());
+
+    accelermoterXAxis.setDouble(ahrs.getRawAccelX());
+    accelermoterYAxis.setDouble(ahrs.getRawAccelY());
+    accelermoterZAxis.setDouble(ahrs.getRawAccelZ());
+
+    gyroYaw.setString(ahrs.getYaw() + " Degrees");
+    gyroPitch.setString(ahrs.getPitch() + " Degrees");
+    gyroRoll.setString(ahrs.getRoll() + " Degrees");
+
+    ahrsIsMoving.setBoolean(ahrs.isMoving());
+    ahrsIsRotating.setBoolean(ahrs.isRotating());
+
+    ahrsAccelX.setString(ahrs.getWorldLinearAccelX() + "");
+    ahrsAccelY.setString(ahrs.getWorldLinearAccelY() + "");
+
+    velocityX.setString(ahrs.getVelocityX() + "");
+    velocityY.setString(ahrs.getVelocityX() + "");
+
+    leftEncoderPulseCount.setString((m_leftLeadDrive.getEncoder().getPosition()*-1) + "");
+    leftEncoderVelocity.setString(m_leftLeadDrive.getEncoder().getVelocity() + "");
+    leftCountsPerRev.setString(m_leftLeadDrive.getEncoder().getCountsPerRevolution() + "");
+
+    rightEncoderPulseCount.setString(m_rightLeadDrive.getEncoder().getPosition() + "");
+    rightEncoderVelocity.setString(m_rightLeadDrive.getEncoder().getVelocity() + "");
+    rightCountsPerRev.setString(m_rightLeadDrive.getEncoder().getCountsPerRevolution() + "");      
+  }
 }
